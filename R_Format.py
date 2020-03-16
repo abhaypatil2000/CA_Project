10535cf81aa11158fb2e4cf62eba709c78c5ffdb
i-navneet <60032662+i-navneet@users.noreply.github.com>
2020-03-16 19:11:34 +0530

    Update R_Format.py

--- R_Format.py
+++ R_Format.py
@@ -2,14 +2,17 @@ import re
 # class IncorrectArgumentError(Error):
 #     pass
 def R_Format(input):
+    global ERROR
     mnemonics = {'add':['0110011','000','0000000'],'and':['0110011','111','0000000'],'or':['0110011','110','0000000'], 'sll':['0110011','001','0000000'],'slt':['0110011','010','0000000'],'sra':['0110011','101','0100000'],'srl':['0110011','101','0000000'],'sub':['0110011','000','0100000'],'xor':['0110011','100','0000000'],'mul':['0110011','000','0000001'],'div':['0110011','100','0000001'],'rem':['0110011','110','0000001']}
     input = (input.split('#'))[0]
     x = re.compile(r'^\s*(?P<mnemonic>\w+)\s+x(?P<rd>[0-31])\s*,\s*x(?P<rs1>[0-31])\s*,\s*x(?P<rs2>[0-31])\s*$')
     if x.search(input) == None:
+        ERROR =True
         return ''
     else:
         y = x.search(input)
         if y.group('mnemonic') not in mnemonics.keys():
+            ERROR = True
             return ''
         else:
             z = mnemonics[y.group('mnemonic')]
