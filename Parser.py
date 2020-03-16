4fc025e8a95b32dd90eb07ef997831b0e08ec427
Babayaga007 <2018csb1121@iitrpr.ac.in>
2020-03-16 22:29:30 +0530

    parser changed

--- Parser.py
+++ Parser.py
@@ -2,13 +2,17 @@ import re
 from common_backend import *
 
 def parse(line):
-    label = re.compile($(\w+)\:)
+    label = re.compile("\A\w+\:")
     label_matches = label.search(line)
 
-    instruction = re.compile(((\w+)\s)+)
+    instruction = re.compile("((\w)+\s)+")
     instruction_matches = instruction.search(line)
 
-    data = re.compile(\.(\w+))
+    data = re.compile("\.(\w+)")
     data_matches = data.search(line)
 
-    
+    print(label_matches)
+    print(instruction_matches)
+
+parse("addi x2 x4 123")
+parse("abc:")
