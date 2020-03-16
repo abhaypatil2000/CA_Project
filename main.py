58418ab565b86ce0f8bc81e45969f7edfcbf0054
Babayaga007 <2018csb1121@iitrpr.ac.in>
2020-03-16 23:13:49 +0530

    main changes

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from common_backend import *
 from Parser import parse
 
 def main1():
-<<<<<<< HEAD
 	file_read = open("input.txt","r")
 	file_write = open("output.mc","w")
 	lines =file_read.readlines()
@@ -20,20 +19,8 @@ def main1():
 	lines = lines2.copy()
 	print(lines)
 	file_write.writelines(lines)
-=======
-	file_read = open("input.txt", "r")
-	file_write = open("output.txt", "w")
-	lines = file_read.readlines()
-	for i in range(len(lines)):
-		parse(lines[i], i+1)
-
-	print(x)
-	file_write.writelines(x)
->>>>>>> 0118070a294add213aed620d45c5c3da22e78cd4
 	file_read.close()
 	file_write.close()
-	#parse(lines)
-
 
 # def main1():
 # 	file_read = open("input.txt", "r")
