0d8882c5beb82e86d140b6b40581cb58f775649e
srikoder <nagasrikar.k@gmail.com>
2020-03-16 20:11:01 +0530

    Parser related

--- Parser.py
+++ Parser.py
@@ -1,1 +1,14 @@
 import re
+from common_backend import *
+
+def parse(line):
+    label = re.compile((\w+)\:)
+    label_matches = label.search(line)
+
+    instruction = re.compile(((\w+)\s)+)
+    instruction_matches = instruction.search(line)
+
+    data = re.compile(\.(\w+))
+    data_matches = data.search(line)
+
+    
