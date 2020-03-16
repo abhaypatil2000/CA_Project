dc756d12f2ab1ec0a67328aabd4542d958147f2f
Babayaga007 <2018csb1121@iitrpr.ac.in>
2020-03-16 23:10:12 +0530

    main changes

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-from UJU_Format import *
+from UJU_format import *
 from I_Format import *
+from R_Format import *
+from SB_format import *
+from S_Format import *
 from common_backend import *
 from Parser import parse
 
 def main1():
-	file_read = open("input.txt", "r")
-	file_write = open("output.txt", "w")
-	lines = file_read.readlines()
+	file_read = open("input.txt","r")
+	file_write = open("output.mc","w")
+	lines =file_read.readlines()
+	print(lines)
+	lines=[val for val in lines if val!='\n']
+	lines2 = []
 	for line in lines:
-		parse(line)
-
-	print(x)
-	file_write.writelines(x)
+		line2=line.replace('\n',' ')
+		lines2.append(line2)
+	lines = lines2.copy()
+	print(lines)
+	file_write.writelines(lines)
 	file_read.close()
 	file_write.close()
+	#parse(lines)
 
+
+# def main1():
+# 	file_read = open("input.txt", "r")
+# 	file_write = open("output.txt", "w")
+# 	lines = file_read.readlines()
+# 	for line in lines:
+# 		parse(line)
+#
+# 	print(x)
+# 	file_write.writelines(x)
+# 	file_read.close()
+# 	file_write.close()
+#
 
 
 def main2():
---
+++ output.mc
@@ -0,0 +1,1 @@
+addi x1 x0 1 addi x2 x0 2 bge x0 x0 label addi x4 x0 5 label: add x3 x2 x1 
