6b237631536bc560cf5c26e4248136f2e1f0f6f8
i-navneet <60032662+i-navneet@users.noreply.github.com>
2020-03-23 13:20:57 +0530

    Added support for optional commas

--- R_Format.py
+++ R_Format.py
@@ -2,18 +2,35 @@ import re
 # class IncorrectArgumentError(Error):
 #     pass
 def R_Format(input):
-    global ERROR
     mnemonics = {'add':['0110011','000','0000000'],'and':['0110011','111','0000000'],'or':['0110011','110','0000000'], 'sll':['0110011','001','0000000'],'slt':['0110011','010','0000000'],'sra':['0110011','101','0100000'],'srl':['0110011','101','0000000'],'sub':['0110011','000','0100000'],'xor':['0110011','100','0000000'],'mul':['0110011','000','0000001'],'div':['0110011','100','0000001'],'rem':['0110011','110','0000001']}
     input = (input.split('#'))[0]
-    x = re.compile(r'^\s*(?P<mnemonic>\w+)\s+x(?P<rd>[0-31])\s*,\s*x(?P<rs1>[0-31])\s*,\s*x(?P<rs2>[0-31])\s*$')
-    if x.search(input) == None:
-        ERROR =True
-        return ''
+    x = re.compile(r'^\s*(?P<mnemonic>\w+)\s+(x(?P<rd>[0-31])\s*(,?\s*x(?P<rs1>[0-31])\s*(,?\s*x(?P<rs2>[0-31])\s*)?)?)?$')
+    y = x.search(input)
+    if y == None:
+        print('Error in statement: ',input)
+        return
+    elif y.group('rd') == None:
+        print('Expected 3 arguments found none')
+        return
+    elif int(y.group('rd')) > 31 or y.group('rd')[0] == '0':
+        print('Register x' + y.group('rd') + 'is not recognized')
+        return
+    elif y.group('rs1') == None:
+        print('Expected 3 arguments but got 1')
+        return
+    elif int(y.group('rs1')) > 31 or y.group('rs1')[0] == '0':
+        print('Register x' + y.group('rs1') + 'is not recognized')
+        return
+    elif y.group('rs2') == None:
+        print('Expected 3 arguments but got 2')
+        return
+    elif int(y.group('rs1')) > 31 or y.group('rs2')[0] == '0':
+        print('Register x' + y.group('rs2') + 'is not recognized')
+        return
     else:
-        y = x.search(input)
         if y.group('mnemonic') not in mnemonics.keys():
-            ERROR = True
-            return ''
+            print(y.group('mnemonic') + ' is not supported')
+            return
         else:
             z = mnemonics[y.group('mnemonic')]
             opcode = z[0]
