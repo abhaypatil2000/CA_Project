c5ca9bf72966710536b8a277ee1f155e1ab1ce79
gurpreet-singh-5000 <56289528+gurpreet-singh-5000@users.noreply.github.com>
2020-03-23 20:55:34 +0530

    Update S_Format.py

--- S_Format.py
+++ S_Format.py
@@ -1,3 +1,5 @@
+class MyException(Exception):
+    pass
 mnemonic = {
   
     'sb': {'opcode': '0100011', 'funct3': '000'},
@@ -15,20 +17,30 @@ def SFormat(instruction):
         instruction_arr.remove('')
     (instruction_arr[2], instruction_arr[3]) = (instruction_arr[3], instruction_arr[2])
     machine_code = []
-    if int(instruction_arr[3]) < 2**11 and int(instruction_arr[3]) >= -(2**11):
-        if(int(instruction_arr[3]))>=0:
-            immediate=bin(int(instruction_arr[3])).replace("0b","").rjust(12,'0')
-        else:
-            immediate=bin(2**12-int(instruction_arr[3])).replace("0b","").rjust(12,'0')
-        imm1=str(immediate)[0:7]
-        imm2= str(immediate)[7:12]
-        machine_code.append(imm1)
+    ###################
+    if(len(instruction_arr)!=4):
+        raise MyException("Expected three arguements!")
+    try:
+        int(instruction_arr[3])
+    except:
+        raise MyException("Offset Value must be an Integer!")
+    if int(instruction_arr[3]) >= 2**11 or int(instruction_arr[3]) < -(2**11):
+        raise MyException("Immediate value out of bounds!")
+    ##################   
+    if(int(instruction_arr[3]))>=0:
+        immediate=bin(int(instruction_arr[3])).replace("0b","").rjust(12,'0')
     else:
-        print("Immediate value not in appropriate bounds")
-        return
-
+        immediate=bin(2**12+int(instruction_arr[3])).replace("0b","").rjust(12,'0')
+        
+    imm1=str(immediate)[0:7]
+    imm2= str(immediate)[7:12]
+    machine_code.append(imm1)
     instruction_arr[1] = instruction_arr[1].replace('x', '')
     instruction_arr[2] = instruction_arr[2].replace('x', '')
+    #########################
+    if(int(instruction_arr[1])>31 or int(instruction_arr[2])>31):
+        raise MyException("Register number must be between 0 and 31, both inclusive.")
+    #########################    
     machine_code.append(bin(int(instruction_arr[1])).replace("0b", "").rjust(5, '0'))
     machine_code.append(bin(int(instruction_arr[2])).replace("0b", "").rjust(5, '0'))
     machine_code.append(mnemonic[instruction_arr[0]]['funct3'])
@@ -37,4 +49,4 @@ def SFormat(instruction):
     machine_hex="{:08x}".format(int(''.join(machine_code),2))
     return "0x"+machine_hex
 
-print(SFormat("sb x1, 10(x2)"))
+print(SFormat("sb x1, 4(x3)"))
