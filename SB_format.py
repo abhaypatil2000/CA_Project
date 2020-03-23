6e740a8cda3ec85cde02ea1cf46e0fdeae71daae
sarveshvhawal007 <56724225+sarveshvhawal007@users.noreply.github.com>
2020-03-23 14:06:43 +0530

    few changes done

--- SB_format.py
+++ SB_format.py
@@ -27,13 +27,14 @@ def SB_format(instruction):
     instruction_arr[2]=instruction_arr[2].replace('x','')
     
     machine_code.append(immediate_rev[11])
-    machine_code.append(immediate_rev[4:10])
+    machine_code.append(immediate_rev[5:10])
     machine_code.append(bin(int(instruction_arr[2])).replace("0b", "").rjust(5, '0'))
     machine_code.append(bin(int(instruction_arr[1])).replace("0b", "").rjust(5, '0'))
     machine_code.append(isa[instruction_arr[0]]['fun3'])
-    machine_code.append(immediate_rev[0:4])
+    machine_code.append(immediate_rev[1:5])
     machine_code.append(immediate_rev[10])
     machine_code.append(isa[instruction_arr[0]]['opcode'])
+    print(machine_code)
     while "" in machine_code:
         machine_code.remove("")
         
@@ -41,4 +42,4 @@ def SB_format(instruction):
     machine_hex="{:08x}".format((int(machine_bin,2)))
     return "0x" + machine_hex
     
-print(SB_format("bne x1,x2,4"))
+print(SB_format("bge x0 x0 12"))
