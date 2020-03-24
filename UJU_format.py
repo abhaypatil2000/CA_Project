46a2f39cacc9f67940279a82cba2a776e1e70cae
Abhay Patil <abhay.patil2000@gmail.com>
2020-03-24 19:31:18 +0530

    exceptions added

--- UJU_format.py
+++ UJU_format.py
@@ -1,27 +1,44 @@
+import re
 mnemonic = {
 		'lui' : {'opcode' : '0110111'},#U format
 		'auipc' : {'opcode' : '0010111'},#UJ format
 		'jal' : {'opcode' : '1101111'}
 		}
 
+class SntxErr(Exception):
+	pass
+
+class BigData(Exception):
+	pass
+
 def UJU_Format(instruction):#check for negative numbers
 	instruction = instruction.replace(" ", ",")
 	ins = instruction.split(",")
 	while "" in ins:
 		ins.remove("")
+	if (len(ins) != 3):
+		raise SntxErr("Wrong number of arguments")
 	opc = ins[0]#mnemonic
 	op = ins[0]#done
 	rd = ins[1]#done
+	if (rd[0] != 'x'):
+		raise SntxErr("No destination register given")
+	
 
 	imm_temp = ins[2]#before the rearrangment or splicing
 	imm_final = ''#after rearrangement
+	
+	for ch in imm_temp:
+		if (ch != [0-9]):
+			raise SntxErr("Not a decimal number")
 	op = str(mnemonic[op]['opcode'])
 	rd = str(bin(int(rd.replace('x', '')))).replace('0b', '').rjust(5, '0')
 	imm_temp = str(bin(int(imm_temp, 0))).replace('0b', '').rjust(32, '0')[11:32]##now we have 21 bits from lsb excl
 
 	if (opc == 'jal'):#exclude the imm[21]
-		imm_final = imm_temp[0:20]# + imm_temp[10:20] + imm_temp[9] + imm_temp[1:9]
-	else:#if lui or addi
+		imm_final = imm_temp[0] + imm_temp[10:20] + imm_temp[9] + imm_temp[1:9]
+		#imm_final = imm_temp[0:20]# + imm_temp[10:20] + imm_temp[9] + imm_temp[1:9]
+	else:#if lui or auipc
 		imm_final = imm_temp[1:21]
 
 	machine_code = imm_final + rd + op
